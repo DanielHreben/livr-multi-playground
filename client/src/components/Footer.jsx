c6067e89fe49037294775d279d528d290a37e94e
Alexey Zaslavsky <pterolex@gmail.com>
2016-06-26 03:09:03 +0300

    Add version

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,6 +5,8 @@ let React = require('react');
 let Col = require('react-bootstrap/lib/Col');
 let Row = require('react-bootstrap/lib/Row');
 
+import { version } from '../../package.json';
+
 require('./Footer.less');
 
 let Footer = React.createClass({
@@ -14,6 +16,11 @@ let Footer = React.createClass({
                 <Col xs={12}>
                     <small>
                         Developed by <a href='http://sdswanderer.github.io/about/' target='_blank'> Dan </a>
+                        and <a href='https://github.com/pterolex' target='_blank'>Alexey</a>
+                    </small>
+                    <br />
+                    <small>
+                        Version {version}
                     </small>
                 </Col>
             </Row>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,5 +4,8 @@ import './assets';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.jsx';
+import { version } from '../package.json';
+
+console.info(`Version ${version}`);
 
 ReactDOM.render( <App/>, document.getElementById('content') );
