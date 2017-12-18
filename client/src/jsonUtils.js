622264ae20f555d36eed1db659cb19a9baf3af1b
Daniel Hreben <daniel.hreben@gmail.com>
2017-12-18 14:11:04 +0200

    Cleanup

--- client/src/jsonUtils.js
+++ client/src/jsonUtils.js
@@ -1,8 +1,6 @@
 import JSON5 from 'json5';
 
 export function parse(json) {
-    console.log(json, '===============!');
-    console.log(JSON5.parse(json), '=============11');
     return JSON5.parse(json);
 }
 
